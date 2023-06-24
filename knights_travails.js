9a9e5469566eb977d0f936dce1d13383adf1bc1f
Nikita <nikita@oden.com>
2023-06-24 23:59:12 +0300

    Get it working

---
+++ knights_travails.js
@@ -0,0 +1,134 @@
+class Queue {
+  constructor() {
+    this.values = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+
+  enqueue(value) {
+    this.values[this.tail++] = value;
+  }
+
+  dequeue() {
+    const value = this.values[this.head];
+    delete this.values[this.head++];
+    return value;
+  }
+
+  clear(){
+    this.values = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+
+  get isEmpty() {
+    return this.head === this.tail;
+  }
+}
+
+class Node {
+  constructor(square, previous) {
+    this.square = square;
+    this.previous = previous || null;
+    this.moves = null;
+  }
+}
+
+class Board {
+  constructor() {
+    this.start = null;
+    this.end = null;
+    this.tree = {};
+    this.board = [...Array(8)].map(() => Array(8).fill(0)); // contains places that the knight have already visited
+    this.possibleMoves = [
+      [2, 1],
+      [2, -1],
+      [1, 2],
+      [1, -2],
+      [-2, 1],
+      [-2, -1],
+      [-1, 2],
+      [-1, -2],
+    ];
+  }
+
+  getPossibleMoves(x, y, previous) {
+    const moves = [];
+    for (let i = 0; i < 8; i++) {
+      const newX = x + this.possibleMoves[i][0];
+      const newY = y + this.possibleMoves[i][1];
+      if (0 <= newX && newX <= 7 && 0 <= newY && newY <= 7 && this.board[newX][newY] != 1) {
+        moves.push(new Node([newX, newY], previous));
+        this.board[newX][newY] = 1;
+      }
+    }
+
+    return moves;
+  }
+
+  findPath() {
+    if (this.start == null && this.end == null) return;
+    this.tree = new Node(this.start);
+    const queue = new Queue();
+    queue.enqueue(this.tree);
+    let currentSquare;
+    while (!queue.isEmpty) {
+      const node = queue.dequeue();
+      node.moves = this.getPossibleMoves(node.square[0], node.square[1], node);
+      for (const move of node.moves){
+        if(this.end[0] == move.square[0] && this.end[1] == move.square[1]) {
+          currentSquare = move;
+          queue.clear();
+          break;
+        } else {
+          queue.enqueue(move);
+        }
+      }
+    }
+    
+    const path = [];
+    while (currentSquare != null) {
+      path.unshift(currentSquare.square);
+      currentSquare = currentSquare.previous;
+    }
+
+    return path;
+  }
+
+  // getPossibleMoves(x, y, moves = new Queue()) {
+  //   for (let i = 0; i < 8; i++) {
+  //     const newX = x + this.possibleMoves[i][0];
+  //     const newY = y + this.possibleMoves[i][1];
+  //     if (0 <= newX && newX <= 7 && 0 <= newY && newY <= 7) {
+  //       moves.enqueue([newX, newY]);
+  //     }
+  //   }
+
+  //   return moves;
+  // }
+
+  // knightMoves() {
+  //   if (this.start == null && this.end == null) return;
+
+  //   const moves = new Queue();
+  //   moves.enqueue(this.start);
+  //   while (!moves.isEmpty) {
+  //     const square = moves.dequeue();
+  //     if (square[0] === this.end[0] && square[1] === this.end[1]) break;
+  //     const moves = this.getPossibleMoves(square[0], square[1]);
+  //     moves.forEach(move => {
+
+  //     });
+  //   }
+
+  //   return [];
+  // }
+}
+
+const board = new Board();
+board.start = [0, 0];
+board.end = [12, 8];
+console.log(board.findPath());
+// console.log(board.knightMoves([0, 0], [3, 3]));
+
+//Dijkstra's algorithm
